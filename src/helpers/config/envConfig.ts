187f0601c5f1694f9c38ee63235ba7071855749e
BayajidAlam <bayajidalam2001@gmail.com>
2024-06-16 00:37:36 +0600

    feat: component created to upload image in imgbb

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,4 +1,10 @@
-export const getBaseUrl = ():string => {
+export const getBaseUrl = (): string => {
+  return process.env.VITE_API_URL || "http://localhost:5000/api/v1";
+};
 
-  return process.env.VITE_API_URL || "http://localhost:5000/api/v1"
-}
+export const getImgBBUrl = (): string => {
+  const imgBBUrl =
+    import.meta.env.imgBB_KEY as string|| "3e7c5c0cd1ac6a06dc8f8e896ce3a95c";
+  console.log(imgBBUrl);
+  return imgBBUrl;
+};
--- src/pages/UploadVideo/UploadVideo.tsx
+++ src/pages/UploadVideo/UploadVideo.tsx
@@ -1,7 +1,7 @@
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Label } from "@radix-ui/react-label";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { Form } from "react-router-dom";
 import { z } from "zod";
 import "react-quill/dist/quill.snow.css";
@@ -10,6 +10,7 @@ import { Button } from "@/components/ui/button";
 import Loading from "@/components/Loading/Loading";
 import { Textarea } from "@/components/ui/textarea";
 import { uploadVideoSchema } from "@/schemas/createEntry";
+import UploadImage from "@/components/UploadFiles/UploadImage";
 
 const UploadVideo = () => {
   const [value, setValues] = useState("");
@@ -20,6 +21,7 @@ const UploadVideo = () => {
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
+  const methods = useForm();
 
   const handleInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter" && inputValue.trim()) {
@@ -42,7 +44,7 @@ const UploadVideo = () => {
   //   resolver: zodResolver(uploadVideoSchema),
   // });
 
-  const form = useForm()
+  const form = useForm();
 
   const {
     register,
@@ -64,179 +66,144 @@ const UploadVideo = () => {
   if (isLoading) return <Loading />;
   return (
     <div>
-      <div className="m-5">
-        <div className="w-1/2 mt-2">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex flex-col  items-start gap-1 mt-2">
-              <Label htmlFor="username" className="text-right">
-                Upload Video
-              </Label>
-
-              <div className="flex items-center justify-center w-full">
-                <label
-                  htmlFor="dropzone-file"
-                  className="flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-                >
-                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg
-                      className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 16"
-                    >
-                      <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                      />
-                    </svg>
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span className="font-semibold">Click to upload</span> or
-                      drag and drop
-                    </p>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">
-                      Video files only (e.g., MP4, MOV)
-                    </p>
-                  </div>
-
-                  <Input
-                    id="dropzone-file"
-                    type="file"
-                    accept="video/*"
-                    className="hidden"
-                    {...register("video")}
-                  />
-                </label>
-              </div>
-              {errors.video && (
-                <p className="text-red-500">{errors.video.message}</p>
-              )}
-            </div>
-            <div className="flex flex-col  items-start gap-1 mt-2">
-              <Label htmlFor="username" className="text-right">
-                Title
-              </Label>
-              <Input
-                id="name"
-                placeholder="New Bangle Song"
-                className="col-span-3"
-                {...register("title")}
-              />
-              {errors.title && (
-                <p className="text-red-500">{errors.title.message}</p>
-              )}
-            </div>
-
-            <div className="flex flex-col  items-start gap-1 mt-2">
-              <Label htmlFor="username" className="text-right">
-                Category
-              </Label>
-              <Input
-                id="name"
-                placeholder="New Bangle Song"
-                className="col-span-3"
-                {...register("title")}
-              />
-              {errors.title && (
-                <p className="text-red-500">{errors.title.message}</p>
-              )}
-            </div>
-
-            <div>
-              <div className="tags-input-container">
+      <FormProvider {...methods}>
+        {" "}
+        <div className="m-5">
+          <div className="w-1/2 mt-2">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex flex-col  items-start gap-1 mt-2">
                 <Label htmlFor="username" className="text-right">
-                  Tags
+                  Upload Video
                 </Label>
-                <div className="flex flex-wrap gap-2">
-                  {tags.map((tag, index) => (
-                    <div key={index} className="tag-item">
-                      <button
-                        className="bg-gray-200 p-1 rounded-lg my-2"
-                        onClick={() => handleDeleteTag(tag)}
+
+                <div className="flex items-center justify-center w-full">
+                  <label
+                    htmlFor="dropzone-file"
+                    className="flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                  >
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                      <svg
+                        className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 16"
                       >
-                        {tag} x
-                      </button>
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                        />
+                      </svg>
+                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                        <span className="font-semibold">Click to upload</span>{" "}
+                        or drag and drop
+                      </p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">
+                        Video files only (e.g., MP4, MOV)
+                      </p>
                     </div>
-                  ))}
+
+                    <Input
+                      id="dropzone-file"
+                      type="file"
+                      accept="video/*"
+                      className="hidden"
+                      {...register("video")}
+                    />
+                  </label>
                 </div>
+                {errors.video && (
+                  <p className="text-red-500">{errors.video.message}</p>
+                )}
+              </div>
+              <div className="flex flex-col  items-start gap-1 mt-2">
+                <Label htmlFor="username" className="text-right">
+                  Title
+                </Label>
                 <Input
-                  type="text"
-                  value={inputValue}
-                  onChange={handleInputChange}
-                  onKeyDown={handleInputKeyDown}
-                  placeholder="Type and press enter to add tags"
+                  id="name"
+                  placeholder="New Bangle Song"
+                  className="col-span-3"
+                  {...register("title")}
                 />
+                {errors.title && (
+                  <p className="text-red-500">{errors.title.message}</p>
+                )}
               </div>
-            </div>
 
-            <div className="flex flex-col items-start gap-1 mt-2">
-              <Label htmlFor="username" className="text-right">
-                Upload Thumbnail
-              </Label>
+              <div className="flex flex-col  items-start gap-1 mt-2">
+                <Label htmlFor="username" className="text-right">
+                  Category
+                </Label>
+                <Input
+                  id="name"
+                  placeholder="New Bangle Song"
+                  className="col-span-3"
+                  {...register("title")}
+                />
+                {errors.title && (
+                  <p className="text-red-500">{errors.title.message}</p>
+                )}
+              </div>
 
-              <div className="flex items-center justify-center w-full">
-                <label
-                  htmlFor="dropzone-file-thumbnail"
-                  className="flex flex-col items-center justify-center w-full h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-                >
-                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg
-                      className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 16"
-                    >
-                      <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                      />
-                    </svg>
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span className="font-semibold">Click to upload</span> or
-                      drag and drop
-                    </p>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">
-                      Image files only (e.g., JPG, PNG)
-                    </p>
+              <div>
+                <div className="tags-input-container">
+                  <Label htmlFor="username" className="text-right">
+                    Tags
+                  </Label>
+                  <div className="flex flex-wrap gap-2">
+                    {tags.map((tag, index) => (
+                      <div key={index} className="tag-item">
+                        <button
+                          className="bg-gray-200 p-1 rounded-lg my-2"
+                          onClick={() => handleDeleteTag(tag)}
+                        >
+                          {tag} x
+                        </button>
+                      </div>
+                    ))}
                   </div>
-
                   <Input
-                    id="dropzone-file-thumbnail"
-                    type="file"
-                    accept="image/*"
-                    className="hidden"
-                    {...register("thumbnail")}
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDown}
+                    placeholder="Type and press enter to add tags"
                   />
-                </label>
+                </div>
               </div>
-              {errors.thumbnail && (
-                <p className="text-red-500">{errors.thumbnail.message}</p>
-              )}
-            </div>
 
-            <div className="flex flex-col items-start gap-1 mt-2">
-              <Label htmlFor="description" className="text-right">
-                Video Description
-              </Label>
-              <Textarea {...register("description")} />
-              {errors.description && (
-                <p className="text-red-500">{errors.description.message}</p>
-              )}
-            </div>
+              <div className="flex flex-col items-start gap-1 mt-2">
+                <Label htmlFor="username" className="text-right">
+                  Upload Thumbnail
+                </Label>
 
-            <div className="text-end mt-5">
-              <Button type="submit">Upload</Button>
-            </div>
-          </form>
+                <UploadImage name="thumbnail" />
+                {errors.thumbnail && (
+                  <p className="text-red-500">{errors.thumbnail.message}</p>
+                )}
+              </div>
+
+              <div className="flex flex-col items-start gap-1 mt-2">
+                <Label htmlFor="description" className="text-right">
+                  Video Description
+                </Label>
+                <Textarea {...register("description")} />
+                {errors.description && (
+                  <p className="text-red-500">{errors.description.message}</p>
+                )}
+              </div>
+
+              <div className="text-end mt-5">
+                <Button type="submit">Upload</Button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      </FormProvider>
     </div>
   );
 };
